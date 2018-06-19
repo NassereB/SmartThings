b42d3f9524a0ef076f6033b04a80ce879b7b562b
Nassere Besseghir <nassereb@gmail.com>
2018-06-19 10:38:42 +0200

    Update automaticScreens.groovy
    Fix dynamic page with params bug

--- automaticScreens.groovy
+++ automaticScreens.groovy
@@ -30,71 +30,114 @@ definition(
 ////////////////////////////
 
 preferences {
-    page(name: "otherPage")
-    page(name: "pageConfigureBlinds")
+    page(name: "pageMain")
+    page(name: "pageConfigureWindowShade")
     page(name: "pageForecastIO")
 }
 
-def otherPage() {
-    dynamicPage(name: "otherPage") {
-        section("Turn on when motion detected:") {
-            input "themotion", "capability.motionSensor", required: true, title: "Where?"
-        }
-        section("Screen:") {
-            input "thescreen", "capability.windowShade", required: true, title: "Which screens?", multiple: true, submitOnChange: true
-            if (thescreen) thescreen.each {
-                href "pageConfigureBlinds", title: "Configure ${it.name}", description: "Tap to open", params: it
+def pageMain() {
+    def thescreen = [
+        name:       "z_blinds",
+        type:       "capability.windowShade",
+        title:      "Which blinds/screens/shutters?",
+        multiple:   true,
+        submitOnChange: true,
+        required:   false
+    ]
+            
+    dynamicPage(name: "pageMain", title: "Configure blinds", install: true, uninstall: true) {
+        section("Shades Control", hideable:true) {
+			paragraph image:"https://raw.githubusercontent.com/verbem/SmartThingsPublic/master/smartapps/verbem/smart-screens.src/Settings.png", "Configure Window Shades"
+            if (thescreen) z_blinds.each { windowShade ->
+                href "pageConfigureWindowShade", title:"${windowShade.displayName}", description:"", params: windowShade
+            }
+        }  
+    
+        section("Screen", hideable:true) {
+            input thescreen
+            if (thescreen) {
+                z_blinds.each { windowShade ->
+                    def devId = windowShade.id
+                    // Settings
+                    paragraph image: "http://cdn.device-icons.smartthings.com/Weather/weather14-icn@2x.png",
+                        title: "Configuration for screen ${windowShade.displayName}",
+                        required: true,
+                        "WindowShadeState: ${windowShade.currentValue("windowShade")}"
+                    input "windowShade_${devId}_orientation", "number", 
+                        title: "Orientation", 
+                        required: true,
+                        defaultValue: 0, 
+                        range: "0..360", 
+                        description: "Orientation of the blinds relative to true (geographic) north (0 = North, 90 = east, 180 = south, 270 = West)"
+                    input "windowShade_${devId}_directionalTolerance", "number", 
+                        title: "Directional tolerance", 
+                        required: true,
+                        defaultValue: 80,
+                        range: "0..90"
+                    input "windowShade_${devId}_directionalToleranceOtherDirection", "number", 
+                        title: "Directional tolerance in the other direction", 
+                        required: true,
+                        defaultValue: 80,
+                        range: "0..90"
+                        
+                    input "windowShade_${devId}_cloudCover", "number", 
+                        title: "Protect until what cloudcover% (0=clear sky)", 
+                        range: "0..100", 
+                        multiple: false, 
+                        required: false, 
+                        defaultValue: 30
+                        
+                    paragraph image: "http://cdn.device-icons.smartthings.com/Weather/weather1-icn@2x.png",
+                        title: "Wind protection",
+                        required: true,
+                        "Beaufort"
+                    input "windowShade_${devId}_cloudCover", "enum", 
+                        title: "Wind speed", 
+                        options: [
+                            "0 - Calm",
+                            "1 - Light air",
+                            "2 - Light breeze",
+                            "3 - Gentle breeze",
+                            "4 - Moderate breeze", 
+                            "5 - Fresch breeze", 
+                            "6 - Strong breeze",
+                            "7 - High wind, moderate gale, near gale",
+                            "8 - Gale, fresh gale",
+                            "9 - Strong/severe gale",
+                            "10 - Storm, whole gale",
+                            "11 - Violent storm",
+                            "12 - Hurricane force"]
+                }
             }
         }
+        
         section("Info Page") {
             href "pageForecastIO", title: "Environment Info", description: "Tap to open"
+        }
+        
+        section("Send Push Notification?") {
+            input "sendPush", "bool", required: false, title: "Send Push Notification?"
         }
     }
 }
 
-def pageConfigureBlinds(dev) {
+def pageConfigureWindowShade(windowShade) {
+    // Workaround: Bug in Dynamic pages with parameters and submitOnChange
+    // See: https://community.smartthings.com/t/bug-in-dynamic-pages-with-parameters-and-submitonchange/36936/4
+    if (windowShade?.id != null) state.dynamicPageParameter = windowShade.id
+    windowShade = z_blinds.findResult { it -> it.id == state.dynamicPageParameter ? it : null }
+
     def pageProperties = [
-        name: "pageConfigureBlinds",
-        title: "Configure for ${dev.name}",
-        nextPage: "pageSetupForecastIO",
+        name: "pageForecastIO",
+        title: "Configure '${windowShade.displayName}'",
+        nextPage: "pageMain",
         uninstall: false
     ]
-
+   
     return dynamicPage(pageProperties) {
-        thescreen.each {
-            log.debug "it.name: ${it.name}"
-            log.debug "it: ${it}"
-            if (it.name == dev.name) {
-                def devId = it.id
-                def devType = it.typeName
-                def blindOptions = ["Down", "Up"]
-
-                if (it.hasCommand("presetPosition")) blindOptions.add("Preset")
-                if (it.hasCommand("stop")) blindOptions.add("Stop")
-
-                def blind = it.currentValue("somfySupported")
-                if (blind == 'true') {
-                    blind = true
-                } else {
-                    blind = false
-                }
-
-                section(it.name) {
-                    paragraph image: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-                        title: "paragraph title",
-                        required: true,
-                        "Orientation of the blinds relative to true (geographic) north (0 = North, 90 = east, 180 = south, 270 = West)"
-                    input "orientation_${devId}", "number", required: false, title: "Orientation",
-                        default: 0
-                    input "directionalTolerance_${devId}", "number", required: false, title: "Directional tolerance",
-                        default: 45
-                    input "directionalToleranceOtherDirection_${devId}", "number", required: false, title: "Directional tolerance in the other direction",
-                        default: 45
-                    input "closeMaxAction_${devId}", "enum", title: "Action to take", options: blindOptions
-                    input "cloudCover_${devId}", "number", title: "Protect until what cloudcover% (0=clear sky)", range: "0..100", multiple: false, required: false,
-                        default: 30
-                }
-            }
+        section("Hub Location") {
+            paragraph "Latitude: ${location.latitude}"
+            paragraph "Longitude: ${location.longitude}"
         }
     }
 }
@@ -105,91 +148,150 @@ def pageForecastIO() {
     def pageProperties = [
         name: "pageForecastIO",
         title: "Current Sun Info",
-        nextPage: "otherPage",
+        nextPage: "pageMain",
         refreshInterval: 10,
         uninstall: false
     ]
 
     return dynamicPage(pageProperties) {
-        section("Hub Location") {
-            paragraph "Latitude ${location.latitude}"
-            paragraph "Longitude ${location.longitude}"
-        }
-        section("Sun Position") {
-            //paragraph "Suncoord ${state.c}"
+        def temperaturScale = location.getTemperatureScale() // Either “F” for fahrenheit or “C” for celsius
 
-            paragraph "Azimuth ${sunPosition.azimuth}"
-            paragraph "Altitude ${sunPosition.altitude}"
+        section("Hub Location") {
+            paragraph "Latitude: ${location.latitude}"
+            paragraph "Longitude: ${location.longitude}"
+        }
+        section("Sun Info") {
+            paragraph "Sunrise: ${getSunriseAndSunset().sunrise}"
+            paragraph "Sunset: ${getSunriseAndSunset().sunset}"
+            paragraph "Azimuth: ${sunPosition.azimuth}"
+            paragraph "Altitude: ${sunPosition.altitude}"
+        }
+        section("Current Weather conditions") {
+	        def currentObservation = getWeatherFeature("conditions")?.current_observation
+            paragraph "Temperature: ${temperaturScale == "F" ? currentObservation.temp_f : currentObservation.temp_c}"
+            paragraph "Weather: ${currentObservation.weather ?: "-"}"
+            paragraph "Icon: ${currentObservation.icon} - ${currentObservation.icon_url}"
+            paragraph "Windspeed: ${temperaturScale == "F" ? currentObservation.wind_mph : currentObservation.wind_kph}"
+            paragraph "Wind direction: ${currentObservation.wind_dir} ${currentObservation.wind_degrees}°"
+            paragraph "Wind degrees: ${currentObservation.wind_degrees}"
+            paragraph "UV: ${currentObservation.UV}"
+        }
+        section("Weather forecast") {
+            def forecast = getWeatherFeature("forecast")?.forecast.simpleforecast.forecastday[0]
+            paragraph "High: ${temperaturScale == "F" ? forecast.high.fahrenheit : forecast.high.celsius}"
+            paragraph "Weather: ${forecast.conditions ?: "-"}"
         }
     }
 }
 
-
-
-
-
-
-
+//////////////////////
+// Pre-defined callbacks
+//////////////////////
 
 def installed() {
-    log.debug "Installed with settings: ${settings}"
-    log.debug "Location: ${location.latitude}"
-    log.debug "Location: ${location}"
+    log.debug "'Automatic Screens' smartapp installed with settings: ${settings}"
 
     initialize()
 }
 
 def updated() {
-    log.debug "Updated with settings: ${settings}"
+    log.debug "'Automatic Screens' smartapp updated with settings: ${settings}"
 
     unsubscribe()
     initialize()
 }
 
 def initialize() {
-    // TODO: subscribe to attributes, devices, locations, etc.
-    subscribe(themotion, "motion.active", motionDetectedHandler)
+    log.debug "'Automatic Screens' smartapp initializing"
+    
+    resetState()
 
-    runEvery1Minute(checkForSun)
+    subscribe(location, "sunset", sunsetHandler)
+    subscribe(location, "sunrise", sunriseHandler)
+    
+    subscribe(z_blinds, "windowShade", windowShadeHandler)
+    
+    if(timeOfDayIsBetween(getSunriseAndSunset().sunrise, getSunriseAndSunset().sunset, new Date(), location.timeZone)) {  
+        runEvery10Minutes(checkForSunHandler)
+    }
+    
+    log.debug "'Automatic Screens' smartapp initialized"
 }
 
 //////////////////////
 // Event handlers
 //////////////////////
 
-def motionDetectedHandler(evt) {
-    log.debug "motionDetectedHandler called: $evt"
-    // thescreen.open()
+def sunsetHandler(evt) {
+    resetState()
+    runEvery10Minutes(checkForSunHandler)
 }
 
-def checkForSun(evt) {
-    log.debug "checkForSun called at ${new Date()}"
+def sunriseHandler(evt) {
+    unschedule(checkForSunHandler)
 }
 
+def checkForSunHandler() {
+    log.debug "checkForSunHandler called at ${new Date()}"
+    def sunPosition = getSunPosition(location.latitude, location.longitude, new Date())
+    
+    z_blinds.each { windowShade ->
+        log.debug "Checking action for windowShade '${windowShade.displayName}'"
+        if(isAngleBetweenRange(sunPosition.azimuth, getShadingRangeSetting(windowShade.id))) {
+            // Execute 'Close' command
+            log.debug "Sun matches direction of windowShade '${windowShade.displayName}'"
+            if(state.windowShadeStates[windowShade.id] != "Closed") {
+                log.debug "execution 'close' command for windowShade '${windowShade.displayName}'"
+                windowShade.close()
+                state.windowShadeStates[windowShade.id] = "Closed"
+                if (sendPush) { sendPush("Closed ${windowShade.displayName}!") }
+            }
+        } else {
+            log.debug "WindowShade '${windowShade.displayName}' does not need protection. No sun."
+        }  
+    }
+}
 
+def windowShadeHandler(evt) {
+    log.debug "WindowShade event occured: value => '${evt.value}'"
+}
+
+def getShadingRangeSetting(deviceId) {
+    return [
+        start: settings."windowShade_${deviceId}_orientation" - settings."windowShade_${deviceId}_directionalToleranceOtherDirection",
+        end: settings."windowShade_${deviceId}_orientation" + settings."windowShade_${deviceId}_directionalTolerance"
+    ]
+}
+
+//////////////////////
+// State
+//////////////////////
+def resetState() {
+    state.windowShadeStates = [:]
+    settings.z_blinds.each { windowShade ->
+        state.windowShadeStates[windowShade.id] = "Empty"
+    }
+}
+
+def isAngleBetweenRange(value, range) {
+    def n = (360 + (value % 360)) % 360
+    def a = (3600000 + range.start) % 360
+    def b = (3600000 + range.end) % 360
+
+    if (a < b) 
+        return a <= n && n <= b
+    return a <= n || n <= b
+}
 
 /*-----------------------------------------------------------------------------------------*/
 /*	Return Sun Azimuth and Alitude for current Time
 /*-----------------------------------------------------------------------------------------*/
 def getSunPosition(latitude, logitude, currentDateTime) {
-    state.lat = location.latitude
-    state.lng = location.longitude
-    state.julian = toJulian(currentDateTime)
-
     def lw = rad() * -location.longitude
-    state.lw = lw
-
     def phi = rad() * location.latitude
-    state.phi = phi
-
     def d = toDays(currentDateTime)
-    state.d = d
-
     def c = sunCoords(d)
-    state.c = c
-
     def H = siderealTime(d, lw) - c.ra
-    state.H = H
 
     def az = azimuth(H, phi, c.dec)
     az = (az * 180 / Math.PI) + 180
